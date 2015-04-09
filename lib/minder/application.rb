af4c8b16fcf76e12dd060b287e1750f6e6d3b2d5
Joseph Method <tristil@gmail.com>
2015-04-09 02:56:15 -0400

    Start using curses
    And clean up processing flow

--- lib/minder/application.rb
+++ lib/minder/application.rb
@@ -1,6 +1,8 @@
 require 'minder/config'
 require 'minder/pomodoro_runner'
 
+require 'curses'
+
 module Minder
   class Application
     attr_accessor :config
@@ -15,38 +17,32 @@ module Minder
     end
 
     def run
-      system('clear')
-      current_action = nil
-
-      current_action = pomodoro_runner.advance_action
-      current_action.start!
+      Curses.noecho
+      Curses.init_screen
+      Curses.timeout = 0
 
       loop do
-        char_code = get_keypress
-        exit if char_code == 3
+        char_code = get_keycode
+
+        event = event_from_keycode(char_code)
+
         current_action = pomodoro_runner.current_action
 
-        if current_action.completed?
-          if char_code == 32
-            new_action = pomodoro_runner.advance_action
-            new_action.start!
-            system("afplay #{ASSETS_LOCATION}/start.wav")
-            print "                                \r"
-          else
-            print "Press space to start next period\r"
-          end
-        elsif current_action.elapsed?
-          system("afplay #{ASSETS_LOCATION}/done.wav")
-          current_action.complete!
-        else
-          update_screen(current_action)
-        end
+        resolution = resolve_action(event, current_action)
+
+        public_send(resolution, current_action)
+
+        Curses.setpos(3, 0)
+        Curses.addstr(' ' * 10)
+        Curses.setpos(3, 0)
+        Curses.refresh
       end
+      Curses.close_screen
     end
 
     def update_screen(action)
-      print "#{action.message}\r"
-      STDOUT.flush
+      Curses.setpos(0,0)
+      Curses.addstr(action.message)
     end
 
     def pomodoro_runner
@@ -56,11 +52,54 @@ module Minder
         long_break_duration: config.long_break_duration)
     end
 
-    def get_keypress
-      system("stty raw -echo") #=> Raw mode, no echo
-      char_code = (STDIN.read_nonblock(1).ord rescue nil)
-      system("stty -raw echo") #=> Reset terminal mode
-      char_code
+    def get_keycode
+      char = Curses.getch
+      char && char.ord
+    end
+
+    def end_interval(current_action)
+      system("afplay #{ASSETS_LOCATION}/done.wav")
+      current_action.complete!
+      update_screen(current_action)
+    end
+
+    def start_interval(current_action)
+      new_action = pomodoro_runner.advance_action
+      new_action.start!
+      system("afplay #{ASSETS_LOCATION}/start.wav")
+      update_screen(new_action)
+    end
+
+    def running(current_action)
+      update_screen(current_action)
+    end
+
+    def waiting(current_action)
+      Curses.setpos(0,0)
+      Curses.addstr('Press space to start next period')
+    end
+
+    def resolve_action(event, current_action)
+      return :start_interval unless current_action
+
+      if current_action.completed?
+        if event == :continue
+          :start_interval
+        else
+          :waiting
+        end
+      elsif current_action.elapsed?
+        :end_interval
+      else
+        :running
+      end
+    end
+
+    def event_from_keycode(keycode)
+      case keycode
+      when 3 then :exit
+      when 32 then :continue
+      end
     end
   end
 end
--- minder.gemspec
+++ minder.gemspec
@@ -16,6 +16,8 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
+  spec.add_dependency 'curses'
+
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
