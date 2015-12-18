3c5e264c9d5230ea331d4118c2e106e71a3ccfe5
Joseph Method <tristil@gmail.com>
2015-12-18 00:06:35 -0500

    WIP

--- lib/minder/database/taskwarrior_database.rb
+++ lib/minder/database/taskwarrior_database.rb
@@ -15,24 +15,35 @@ module Minder
     end
 
     def tasks_filtered_by(text)
+      tw.some(dom: { description: text }).map do |task|
+        build_task(task)
+      end
     end
 
     def add_task(text)
+      tw.add!(text)
     end
 
     def delete_task(task)
+      tw.delete!(ids: task.id)
     end
 
     def complete_task(task)
+      tw.done!(ids: task.id)
     end
 
     def start_task(task)
+      tw.start!(ids: task.id)
     end
 
     def unstart_task(task)
+      tw.stop!(ids: task.id)
     end
 
     def update_task(task, options = {})
+      options.each do |key, value|
+        tw.modify!(key, value, ids: task.id)
+      end
     end
 
     def complete_period(task)
@@ -42,22 +53,25 @@ module Minder
     end
 
     def last_period
+      Minder::IdlePeriod.new({})
     end
 
     def update_period(period, options = {})
     end
 
     def periods
+      []
     end
 
     def pomodoros_today
+      []
     end
 
     private
 
     def build_task(tw_task)
       options = {
-        id: tw_task.id,
+        id: tw_task.uuid,
         description: tw_task.description,
         completed_at: tw_task.end,
         started_at: tw_task.start
